19debb34e29061625c5287f5bab22bcc4acea264
Zach Banks <zjbanks@gmail.com>
2021-04-06 17:02:38 -0400

    Matcher results are Vec<Word> to retain score info

--- noodle/src/matcher.rs
+++ noodle/src/matcher.rs
@@ -1,10 +1,8 @@
-use crate::bitset::{BitSet1D, BitSet3D};
+use crate::bitset::BitSet3D;
 use crate::expression::Expression;
 use crate::parser;
 use crate::words::{Char, Word};
-use indexmap::{IndexMap, IndexSet};
-use std::collections::HashSet;
-use std::time;
+use indexmap::IndexMap;
 
 /// Evaluate a query, consisting of multiple expressions, on a given wordset.
 /// Returns words and phrases that match *all* of the given expressions.
@@ -130,11 +128,6 @@ impl<'word> Matcher<'word> {
             wl = &cache.nonnull_wordlist;
         }
 
-        // Empty out the wordlist, if any remaining part of this function fails, then there are
-        // no phrase matches possible
-        self.singles_done = true;
-        self.wordlist = vec![];
-
         // RUNTIME: O(expressions * words)
         let nonnull_wordlist = self
             .cache_builders
@@ -147,8 +140,7 @@ impl<'word> Matcher<'word> {
             .cache_builders
             .drain(..)
             .map(|c| c.finalize(&nonnull_wordlist))
-            .collect::<Result<Vec<_>, _>>()
-            .ok()?;
+            .collect();
 
         // RUNTIME: O(expressions * fuzz * states)
         let fuzz_max = self
@@ -168,22 +160,18 @@ impl<'word> Matcher<'word> {
             .collect();
 
         for (i, cache) in self.caches.iter().enumerate() {
-            if cache.is_dfa {
-                debug_assert_eq!(cache.expression.fuzz, 0);
-                *layers[0].states.slice_mut((i, 0)).as_block_mut() = 1_u32;
-            } else {
-                layers[0].states.slice2d_mut(i).clear();
+            layers[0].states.slice2d_mut(i).clear();
 
-                // TODO: Expand epsilon_states(0) out to states_max
-                let mut starting_states = layers[0].states.slice_mut((i, 0));
-                starting_states.union_with(cache.expression.epsilon_states(0));
-            }
+            // TODO: Expand epsilon_states(0) out to states_max
+            let mut starting_states = layers[0].states.slice_mut((i, 0));
+            starting_states.union_with(cache.expression.epsilon_states(0));
         }
 
         // TODO: This wordlist copy is also potentially more expensive than nessassary,
         // but it keeps the type of `Matcher::wordlist` simple (`Vec<_>` not `Rc<RefCell<Vec<_>>>`)
         self.wordlist = nonnull_wordlist.clone();
         self.layers = layers;
+        self.singles_done = true;
         None
     }
 
@@ -214,63 +202,47 @@ impl<'word> Matcher<'word> {
                         break;
                     }
 
-                    let (table, class) = cache
+                    let class = cache
                         .classes
                         .get_index(cache.word_classes[word_index])
-                        .unwrap();
-                    let fuzz_limit = cache.expression.fuzz + 1;
+                        .unwrap()
+                        .0;
+                    next_layer.states.slice2d_mut(c).clear();
 
+                    // RUNTIME: O(fuzz^2 * states^2)
+                    let fuzz_limit = cache.expression.fuzz + 1;
+                    for f in 0..fuzz_limit {
+                        // RUNTIME: O(fuzz * states^2)
+                        for si in this_layer.states.slice((c, f)).ones() {
+                            // RUNTIME: O(fuzz * states)
+                            let mut fd = 0;
+                            while f + fd < fuzz_limit {
+                                // RUNTIME: O(states)
+                                next_layer
+                                    .states
+                                    .slice_mut((c, f + fd))
+                                    .union_with(class.slice((si, fd)));
+                                fd += 1;
+                            }
+                        }
+                    }
+
+                    // RUNTIME: O(fuzz * states)
                     let mut all_empty = true;
                     let mut all_subset = true;
                     let mut any_end_match = false;
-                    if cache.is_dfa {
-                        debug_assert_eq!(fuzz_limit, 1);
-                        let si = this_layer.states.slice((c, 0)).as_block();
-                        let new_state = class.set_table[si as usize];
-                        *next_layer.states.slice_mut((c, 0)).as_block_mut() = new_state as u32;
-
-                        if cache.terminal_states.contains(&new_state) {
+                    let success_index = cache.expression.states_len() - 1;
+                    for f in 0..fuzz_limit {
+                        let es = next_layer.states.slice((c, f));
+                        if es.contains(success_index) {
                             any_end_match = true;
                             all_empty = false;
                             all_subset = false;
-                        } else if new_state != 0 {
-                            // TODO: implement all_subset (oof)
-                            all_subset = false;
-                            all_empty = false;
-                        }
-                    } else {
-                        // RUNTIME: O(fuzz^2 * states^2)
-                        next_layer.states.slice2d_mut(c).clear();
-                        for f in 0..fuzz_limit {
-                            // RUNTIME: O(fuzz * states^2)
-                            for si in this_layer.states.slice((c, f)).ones() {
-                                // RUNTIME: O(fuzz * states)
-                                let mut fd = 0;
-                                while f + fd < fuzz_limit {
-                                    // RUNTIME: O(states)
-                                    next_layer
-                                        .states
-                                        .slice_mut((c, f + fd))
-                                        .union_with(table.slice((si, fd)));
-                                    fd += 1;
-                                }
-                            }
-                        }
-
-                        // RUNTIME: O(fuzz * states)
-                        let success_index = cache.expression.states_len() - 1;
-                        for f in 0..fuzz_limit {
-                            let es = next_layer.states.slice((c, f));
-                            if es.contains(success_index) {
-                                any_end_match = true;
-                                all_empty = false;
+                        } else if !es.is_empty() {
+                            if !es.is_subset(&this_layer.states.slice((c, f))) {
                                 all_subset = false;
-                            } else if !es.is_empty() {
-                                if !es.is_subset(&this_layer.states.slice((c, f))) {
-                                    all_subset = false;
-                                }
-                                all_empty = false;
                             }
+                            all_empty = false;
                         }
                     }
 
@@ -418,17 +390,12 @@ struct Cache {
     // This is a parallel vector to `wordlist`: for each word in the wordlist, which
     // class does it belong to (by index)?
     word_classes: Vec<usize>,
-
-    terminal_states: HashSet<usize>,
-    is_dfa: bool,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct CacheClass {
     // TODO: In future optimizations, it may be useful to store other information here
     n_words: usize,
-    first_word: Option<String>,
-    set_table: Vec<usize>,
 }
 
 /// This struct is used while doing the first pass over wordlist, looking for single-word
@@ -476,14 +443,7 @@ impl<'word> CacheBuilder<'word> {
         };
 
         // The first class (0) is always the "null" class for words which do not match
-        classes.insert_full(
-            transition_group_new(),
-            CacheClass {
-                n_words: 0,
-                first_word: None,
-                set_table: vec![],
-            },
-        );
+        classes.insert_full(transition_group_new(), CacheClass { n_words: 0 });
 
         let transition_table = vec![transition_group_new(); word_len_max];
 
@@ -492,8 +452,6 @@ impl<'word> CacheBuilder<'word> {
                 expression,
                 classes,
                 word_classes,
-                is_dfa: false,
-                terminal_states: HashSet::new(),
             },
 
             word_index: 0,
@@ -559,17 +517,13 @@ impl<'word> CacheBuilder<'word> {
             let entry = self
                 .cache
                 .classes
-                .entry(self.transition_table[word_len].clone());
-            //.and_modify(|v| v.n_words += 1);
+                .entry(self.transition_table[word_len].clone())
+                .and_modify(|v| v.n_words += 1);
             if entry.index() != 0 {
                 self.nonnull_wordlist.push(word);
                 self.cache.word_classes.push(entry.index());
             }
-            entry.or_insert(CacheClass {
-                n_words: 0,
-                first_word: None,
-                set_table: vec![],
-            });
+            entry.or_insert(CacheClass { n_words: 1 });
 
             // RUNTIME: O(fuzz)
             for f in fuzz_range.clone() {
@@ -588,7 +542,7 @@ impl<'word> CacheBuilder<'word> {
     /// `new_wordlist` must be a subset of the original wordlist (in the exact same order)
     ///
     /// `RUNTIME: O(words)`
-    fn finalize(mut self, new_wordlist: &[&'word Word]) -> Result<Cache, ()> {
+    fn finalize(mut self, new_wordlist: &[&'word Word]) -> Cache {
         assert!(self.single_fully_drained);
         // TODO: There should be an API for stats
         //println!(
@@ -597,13 +551,11 @@ impl<'word> CacheBuilder<'word> {
         //    self.total_matched - self.total_prefixed,
         //    self.total_length - self.total_matched
         //);
-        println!(
-            "{} distinct classes with {} words",
-            self.cache.classes.len(),
-            self.nonnull_wordlist.len(),
-        );
-
-        println!("finalizing: {:?}", self.cache.expression);
+        //println!(
+        //    "{} distinct classes with {} words",
+        //    self.cache.classes.len(),
+        //    self.nonnull_wordlist.len(),
+        //);
 
         let mut new_word_classes = vec![];
         let mut i: usize = 0;
@@ -616,10 +568,6 @@ impl<'word> CacheBuilder<'word> {
         {
             if i < new_wordlist.len() && *word == *new_wordlist[i] {
                 new_word_classes.push(class);
-                self.cache.classes[class].n_words += 1;
-                self.cache.classes[class]
-                    .first_word
-                    .get_or_insert_with(|| word.text.clone());
                 i += 1;
             }
         }
@@ -627,217 +575,7 @@ impl<'word> CacheBuilder<'word> {
         self.cache.word_classes = new_word_classes;
         assert!(self.cache.word_classes.len() == new_wordlist.len());
 
-        //for (i, (table, class)) in self.cache.classes.iter().enumerate() {
-        //    println!(
-        //        "class {}, n_words={}, ex={:?}: {:?}",
-        //        i, class.n_words, class.first_word, table
-        //    );
-        //}
-
-        let states_len = self.cache.expression.states_len();
-        /*
-        let start = time::Instant::now();
-        let mut duplicate_states: Vec<Option<usize>> = vec![None; states_len];
-        for i in 0..states_len {
-            'outer: for j in i + 1..states_len {
-                if duplicate_states[j].is_some() {
-                    continue;
-                }
-                let mut inputs_same = true;
-                let mut outputs_same = true;
-                for f in 0..=self.cache.expression.fuzz {
-                    for (transition_table, class) in self.cache.classes.iter() {
-                        if class.n_words == 0 {
-                            continue;
-                        }
-                        if transition_table.slice((i, f)) != transition_table.slice((j, f)) {
-                            outputs_same = false;
-                        }
-                        for k in 0..states_len {
-                            let slice = transition_table.slice((k, f));
-                            if slice.contains(i) != slice.contains(j) {
-                                inputs_same = false;
-                                break;
-                            }
-                        }
-                        if !inputs_same && !outputs_same {
-                            continue 'outer;
-                        }
-                    }
-                }
-                if inputs_same {
-                    duplicate_states[j] = Some(i);
-                }
-            }
-        }
-        println!(
-            "checked for identical states in {:?}: {:?}",
-            start.elapsed(),
-            duplicate_states
-        );
-
-        let start = time::Instant::now();
-        // TODO: Check that the last state (success) is still the last state
-        let new_states_len = duplicate_states.iter().filter(|x| x.is_none()).count();
-        if new_states_len != states_len {
-            let new_state_index: Vec<_> = duplicate_states
-                .iter()
-                .enumerate()
-                .map(|(i, x)| {
-                    if x.is_some() {
-                        None
-                    } else {
-                        Some(duplicate_states[..i].iter().filter(|y| y.is_none()).count())
-                    }
-                })
-                .collect();
-
-            let transition_group_new = || {
-                BitSet3D::new(
-                    (new_states_len, self.cache.expression.fuzz + 1),
-                    new_states_len,
-                )
-            };
-            let mut new_classes = IndexMap::new();
-            new_classes.insert_full(
-                transition_group_new(),
-                CacheClass {
-                    n_words: 0,
-                    first_word: None,
-                    set_table: vec![],
-                },
-            );
-
-            for (index, (transition_table, class)) in self.cache.classes.iter().enumerate() {
-                if class.n_words == 0 {
-                    continue;
-                }
-                let mut new_table = transition_group_new();
-                for (i, &i_new) in new_state_index.iter().enumerate() {
-                    let i_new = i_new
-                        .unwrap_or_else(|| new_state_index[duplicate_states[i].unwrap()].unwrap());
-                    for f in 0..=self.cache.expression.fuzz {
-                        let old_slice = transition_table.slice((i, f));
-                        let mut new_slice = new_table.slice_mut((i_new, f));
-                        for j in old_slice.ones() {
-                            if let Some(new_j) = new_state_index[j] {
-                                new_slice.insert(new_j);
-                            }
-                        }
-                    }
-                }
-
-                new_classes
-                    .entry(new_table)
-                    .and_modify(|c| c.n_words += class.n_words)
-                    .or_insert(class.clone());
-            }
-            for (i, (table, class)) in new_classes.iter().enumerate() {
-                println!(
-                    "new_class {} with {} words first {:?}: {:?}",
-                    i, class.n_words, class.first_word, table
-                );
-            }
-
-            let mut new_state_powerset = IndexSet::new();
-            for table in new_classes.keys() {
-                for i in 0..new_states_len {
-                    for f in 0..=self.cache.expression.fuzz {
-                        new_state_powerset.insert(table.slice((i, f)));
-                    }
-                }
-            }
-            println!("new powerset size={}", new_state_powerset.len());
-
-            // TODO: remap word
-            println!("remapped states in {:?}", start.elapsed());
-        }
-
-        let mut state_powerset = IndexSet::new();
-        for table in self.cache.classes.keys() {
-            for i in 0..new_states_len {
-                for f in 0..=self.cache.expression.fuzz {
-                    state_powerset.insert(table.slice((i, f)));
-                }
-            }
-        }
-        println!("old powerset size={}", state_powerset.len());
-        */
-
-        let copy_bitset = |bref| {
-            let mut bitset = BitSet1D::new((), states_len);
-            bitset.borrow_mut().copy_from(bref);
-            bitset
-        };
-
-        let start = time::Instant::now();
-        let mut setmap = IndexSet::new();
-        // The "null" state
-        setmap.insert(copy_bitset(
-            self.cache.classes.get_index(0).unwrap().0.slice((0, 0)),
-        ));
-        // The starting state
-        setmap.insert(copy_bitset(self.cache.expression.epsilon_states(0)));
-
-        self.cache.is_dfa = self.cache.expression.fuzz == 0;
-        if self.cache.is_dfa {
-            for (_table, mut class) in self.cache.classes.iter_mut() {
-                if class.n_words != 0 {
-                    class.set_table = vec![0];
-                }
-            }
-
-            let mut s = 1;
-            while s < setmap.len() {
-                let from_set = setmap.get_index(s).unwrap().clone();
-                for (table, class) in self.cache.classes.iter_mut() {
-                    if class.n_words == 0 {
-                        continue;
-                    }
-                    for f in 0..=self.cache.expression.fuzz {
-                        let mut to_set = BitSet1D::new((), states_len);
-                        for i in from_set.borrow().ones() {
-                            to_set.borrow_mut().union_with(table.slice((i, f)));
-                        }
-                        let (index, _new) = setmap.insert_full(to_set);
-                        debug_assert_eq!(class.set_table.len(), s);
-                        class.set_table.push(index);
-                    }
-                }
-                if setmap.len() > 1024 {
-                    self.cache.is_dfa = false;
-                    break;
-                }
-                s += 1;
-            }
-        }
-
-        if self.cache.is_dfa {
-            for (index, set) in setmap.iter().enumerate() {
-                if set.borrow().contains(states_len - 1) {
-                    self.cache.terminal_states.insert(index);
-                }
-            }
-            if self.cache.terminal_states.is_empty() {
-                println!("-- Terminal states are unreachable; there are not matches! --");
-                return Err(());
-            }
-
-            let dt = start.elapsed();
-            println!(
-                "Converted NFA with {} states to DFA with {} states in {:?}",
-                states_len,
-                setmap.len(),
-                dt
-            );
-            //for (index, set) in setmap.iter().enumerate() {
-            //    println!("{}  {:?}", index, set);
-            //}
-            //println!("terminal states: {:?}", self.cache.terminal_states);
-            //println!("dfa time = {:?}", dt);
-        }
-
-        Ok(self.cache)
+        self.cache
     }
 
     fn iter<'it>(&'it mut self, wordlist: &'it [&'word Word]) -> CacheBuilderIter<'word, 'it> {
